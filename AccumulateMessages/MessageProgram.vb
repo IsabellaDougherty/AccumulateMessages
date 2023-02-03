d23a71620d857202ec817237bba40d991ff698a7
Isabella <belladougherty@gmail.com>
2023-02-02 19:35:32 -0700

    Got the code to work, all tests passed

--- AccumulateMessages/MessageProgram.vb
+++ AccumulateMessages/MessageProgram.vb
@@ -3,14 +3,21 @@ Imports System
 
 Module MessageProgram
     Sub Main(args As String())
-        'uncomment to test interactively
         'Test.Manual()
         Test.Auto()
     End Sub
 
     Function UserMessages(ByVal newMessage As String, ByVal clear As Boolean) As String
-        'your code here
-        Return messages
+        Static message As String
+        If clear Then
+            message = ""
+        ElseIf ((newMessage.Length < 1)) Then
+
+        Else
+            message += newMessage + "
+"
+        End If
+        Return message
     End Function
 
 
